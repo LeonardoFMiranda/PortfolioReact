f3fa21df56b5cefd1844e4c413a26ceeb19113cf
LeonardoFMiranda <leomiranda1997@gmail.com>
2025-01-30 13:32:30 -0300

    Atualizacao da pagina 404

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,17 +1,19 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, } from 'react-router-dom';
 import Home from '../components/pages/Home/Home';
 import Portfolio from '../components/pages/Portfolio/Portfolio';
 import Certificados from '../components/pages/Certificados/Certificados';
 import Aboutme from '../components/pages/Aboutme/Aboutme';
+import NotFound from '../components/pages/NotFound/NotFound';
 
 function Rotas() {
 
     const routes = (
         <Routes>
             <Route path='/' element={<Home />}></Route>
-            <Route path='/portfólio' element={<Portfolio />}></Route>
+            <Route path='/portfolio' element={<Portfolio />}></Route>
             <Route path='/certificados' element={<Certificados />}></Route>
             <Route path='/sobre-mim' element={<Aboutme />}></Route>
+            <Route path='*' element={<NotFound />}></Route>
         </Routes>
     )
 
